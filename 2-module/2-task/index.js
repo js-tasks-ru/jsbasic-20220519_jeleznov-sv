15b2eb14b3bc40fd2c4cb45ab0349652f95e7bba
SergeyZheleznov <jeleznov_sv@mail.ru>
2022-05-30 11:44:36 +0300

    Решение задач занятие1 и занятие2 дата 30.05.2022

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,10 @@
-function isEmpty(obj) {
+function isEmpty(schedule) {
   // ваш код...
+  let arrayTest = Object.keys(schedule);
+
+  if (arrayTest.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
