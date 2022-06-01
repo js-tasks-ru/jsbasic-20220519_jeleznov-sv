29b9a98153d6bd5bd71d08d95eac5ec1452b0b40
SergeyZheleznov <jeleznov_sv@mail.ru>
2022-06-01 08:47:19 +0300

    Решение задач из раздела 3 дата 01.06.2022

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,20 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let filterFunc = (num) => {
+    return num > a && num < b;
+  };
+
+  let filterNums = arr.filter(filterFunc);
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] == a) {
+      filterNums.push(a);
+    }
+
+    if (arr[i] == b) {
+      filterNums.push(b);
+    }
+  }
+
+  return filterNums;
 }
